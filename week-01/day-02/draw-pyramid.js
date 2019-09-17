9a84efa580d552f68d849d396f51bf8729b5d7fa
Cathy Chen <Cathy_Chen@epam.com>
2019-09-17 17:08:20 +0800

    second

--- week-01/day-02/draw-pyramid.js
+++ week-01/day-02/draw-pyramid.js
@@ -17,8 +17,15 @@ const lineCount = 4;
 var m =[];
 for (var i = 1; i<=lineCount; i++){
     for (var j = 0; j < (lineCount-1)*2+1; j++ ){
-        m.push(0);
+        m.push('*');
     }
-    console.log(m);
+    
+    for (var n=0; n<lineCount-i; n++){
+        m[n] = ' ';
+        m[m.length-n-1] = ' ';
+    }
+    var str = m.join('');
+    console.log(str);
     m = [];
 }
+
