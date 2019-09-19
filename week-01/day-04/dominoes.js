fe966cf6a4de6ea6f4acae17840b24d178abd094
Cathy Chen <Cathy_Chen@epam.com>
2019-09-19 19:45:46 +0800

    11

--- week-01/day-04/dominoes.js
+++ week-01/day-04/dominoes.js
@@ -14,8 +14,30 @@ function initializeDominoes() {
 }
 
 const dominoes = initializeDominoes();
+console.log(dominoes);
+console.log(dominoes.length);
+console.log(dominoes[1].values[0]);
+let arr = [];
+
+arr.push(dominoes[0]);
+
+
+for (var i=1; i<dominoes.length; i++){
+    
+    for (var j=1; j<dominoes.length; j++){
+        if(dominoes[j].values[0]==arr[i-1].values[1]){
+
+            arr.push(dominoes[j]);
+      
+        }else{
+            continue
+        }
+    }
+}
+
+
 /** You have the list of Dominoes */
 /** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
 /** eg: [2, 4], [4, 3], [3, 5] ... */
-
+console.log(arr.toString());
 console.log(dominoes.toString());
