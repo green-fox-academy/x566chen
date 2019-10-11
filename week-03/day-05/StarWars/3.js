13e84e6e15c93a799270a1c17320985ff1e6aeda
x566chen <x566chen@uwaterloo.ca>
2019-10-12 00:08:40 +0800

    00

--- week-03/day-05/StarWars/3.js
+++ week-03/day-05/StarWars/3.js
@@ -1,22 +1,76 @@
 //https://swapi.co/api/people/?search=r2
 
-let api = "https://swapi.co/api/people/?search=";
-let ipt ;
+
+
 const btn = document.querySelector('#btn');
-const input = document.querySelector('input');
-
-
-const pic = document.getElementById('pic');
-
-let httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = callback;
-httpRequest.open('GET', `https://swapi.co/api/people/?search=${ipt}`,true);
-httpRequest.send(null);
-
 let ipt = document.getElementById('input');
 
+
 btn.addEventListener('click', ()=>{
-    ipt.value
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = callback;
+    alert(ipt.value);
+    httpRequest.open('GET', `https://swapi.co/api/people/?search=${ipt.value}`,true);
+    httpRequest.send(null);
+
+
+    function callback(){
+        if (httpRequest.readyState == 4 )
+            if (httpRequest.status == 200){
+                let json = httpRequest.responseText;
+                let json_obj = JSON.parse(json);
+
+                console.log(json_obj);
+
+                
+                
+                for (let i=0; i<json_obj.count;i++){
+                    const li = document.createElement('li');
+                    const left =document.querySelector('#left');
+                    left.appendChild(li);
+                    li.setAttribute('id',`li${i}`);
+                    li.innerHTML = json_obj.results[i].name;
+                }  
+                
+                left.addEventListener('click',()=>{
+                    for (let i=0; i<json_obj.count;i++){
+                        for(let j=0; j<json_obj.results[i].films.length; j++){
+                            let Url = json_obj.results[i].films[j];
+                    
+                            let htp = new XMLHttpRequest();
+                            htp.onreadystatechange = callback;
+                    
+                            htp.open('GET', `${Url}`,true);
+                            htp.send(null);
+                    
+                            function callback(){
+                    
+                                if (htp.readyState == 4 )
+                                if (htp.status == 200){
+                                    let json1 = htp.responseText;
+                                    let json_obj1 = JSON.parse(json1);
+                    
+                                    console.log(json_obj1);
+                    
+                                    const li1 = document.createElement('li');
+                                    const right = document.querySelector('#right');
+                                    right.appendChild(li1);
+                                    li1.innerHTML = `${json_obj1.title} (${json_obj1.release_date})`;
+                                }   
+                            }
+                        }
+
+                    }
+
+                    
+
+                    
+                })
+
+                }
+
+            }
+
 })
 
 
@@ -25,20 +79,3 @@ btn.addEventListener('click', ()=>{
 
 
 
-// function callback(){
-//     if (httpRequest.readyState == 4 )
-//         if (httpRequest.status == 200){
-//             let json = httpRequest.responseText;
-//             let json_obj = JSON.parse(json);
-//             for (let i=0; i<16;i++){
-//                 let img = document.createElement('img');
-//                 //let pic=;
-//                 img.setAttribute('src',`${json_obj.data[i].images.original_still.url}`);
-//                 pic.appendChild(img);
-//                 img.addEventListener('click',()=>{
-//                     img.src = json_obj.data[i].images.original.url;
-//                 })
-//             }
-
-//         }
-// }
