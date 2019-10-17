ddc563506fc8d75622c7ed2762c57e3f3a640d2b
x566chen <x566chen@uwaterloo.ca>
2019-10-17 09:48:49 +0800

    22

--- week-04/day-03/basic/basicapp/src/App.js
+++ week-04/day-03/basic/basicapp/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './App.css'
 
 // 73e0a905efdb4303939f186e4605c94c
 //https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=73e0a905efdb4303939f186e4605c94c
 
-const new_url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=73e0a905efdb4303939f186e4605c94c'
+const new_url = 'https://newsapi.org/v2/top-headlines?country=cn&apiKey=73e0a905efdb4303939f186e4605c94c'
 const slow_url = `https://lofty-partner.glitch.me/delay/3000/url/${new_url}`
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state={
       articles:[],
-      //loaded: false
+      loaded: false
     }
   }
 
@@ -18,8 +19,8 @@ class App extends React.Component{
     let response = await fetch(slow_url)
      .then(response =>response.json())
      await this.setState({
-        articles : [...response.articles]
-       //loaded: true
+        articles : [...response.articles],
+        loaded: true
       
      })
      console.log(JSON.stringify(response.articles[0]))
@@ -32,14 +33,21 @@ class App extends React.Component{
 
 
   render(){
-    return (
-      <div className='App'>
-       <ul>
-       {this.state.articles.map(article => (
-            <p key = {article.author}>{article.title}</p>))}
-       </ul>
-      </div>
-    )
+
+    if (this.state.loaded==true){
+      return (
+        <div className='App'>
+         <ul>
+              {this.state.articles.map(article => (
+              <p key = {article.author}>{article.title}</p>))}
+         </ul>
+        </div>
+      )
+}else{
+  return(<div className="lds-heart">1<div></div></div>)
+  
+}
+
   }
   
 
