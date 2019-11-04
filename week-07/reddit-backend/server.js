708283b7457ec3b30d13eda89bbc896ac770661d
x566chen <x566chen@uwaterloo.ca>
2019-11-04 17:40:59 +0800

    backend finish

---
+++ week-07/reddit-backend/server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const mysql = require('mysql');
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'cathy.com',
+    database: 'reddit',
+});
+
+app.get('/posts', (req, res) => {
+    let select_all = 'SELECT * FROM info';
+    connection.query(select_all, (error, result) => {
+        if(error){
+            console.log(error);
+            res.status(500).send('DB ERROR');
+            return;
+        }
+        res.status(200).send({posts: result});
+    })
+});
+
+app.post('/posts', (req,res) => {
+
+    let title = req.body.title;
+    let url = req.body.url;
+    let timestamp = Date.parse(new Date())/1000;
+    let score = 0;
+
+    let add_post = `INSERT INTO info (title, url, timestamp, score)
+                VALUES 
+                ( "${title}", "${url}","${timestamp}", "${score}")`;
+    connection.query(add_post,(error, result)=>{
+
+        if(error){
+        console.log(error);
+        res.status(500).send('DB ERROR');
+        return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(201).send('sucess!');
+    })
+})
+app.put('/posts/:id/upvote', async(req,res) =>{
+    let id = req.params.id;
+    let add_upvote = `UPDATE info SET score=score+1 WHERE id = ${id}`;
+
+    await connection.query(add_upvote,(error, result) =>{
+        if (error){
+            console.log(error);
+        }
+        console.log(result);
+    })
+
+    let select_upvote = `SELECT * FROM info`
+
+    connection.query(select_upvote, (error, result)=>{
+        if(error){
+            console.log(error);
+            res.status(500).send('DB ERROR');
+            return;
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(201).send({posts: result});
+    })
+
+})
+
+app.put('/posts/:id/downvote', async(req,res) =>{
+    let id = req.params.id;
+    let add_upvote = `UPDATE info SET score=score-1 WHERE id = ${id}`;
+
+    await connection.query(add_upvote,(error, result) =>{
+        if (error){
+            console.log(error);
+        }
+        console.log(result);
+    })
+
+    let select_upvote = `SELECT * FROM info`
+
+    connection.query(select_upvote, (error, result)=>{
+        if(error){
+            console.log(error);
+            res.status(500).send('DB ERROR');
+            return;
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.status(201).send({posts: result});
+    })
+
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
